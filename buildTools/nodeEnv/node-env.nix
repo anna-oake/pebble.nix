7ab7996fb3f96fcda8b94adf31fd8d5b6e7d4e7d
Anna Oake <anna@oa.ke>
2026-01-15 19:12:48 +0100

    nodeEnv: fix evaluation warning

--- buildTools/nodeEnv/node-env.nix
+++ buildTools/nodeEnv/node-env.nix
@@ -13,9 +13,6 @@
 }:
 
 let
-  # Workaround to cope with utillinux in Nixpkgs 20.09 and util-linux in Nixpkgs master
-  utillinux = if pkgs ? utillinux then pkgs.utillinux else pkgs.util-linux;
-
   python = if nodejs ? python then nodejs.python else python2;
 
   # Create a tar wrapper that filters all the 'Ignoring unknown extended header keyword' noise
@@ -545,15 +542,14 @@ let
     stdenv.mkDerivation (
       {
         name = "${name}${if version == null then "" else "-${version}"}";
-        buildInputs =
-          [
-            tarWrapper
-            python
-            nodejs
-          ]
-          ++ lib.optional (stdenv.isLinux) utillinux
-          ++ lib.optional (stdenv.isDarwin) libtool
-          ++ buildInputs;
+        buildInputs = [
+          tarWrapper
+          python
+          nodejs
+        ]
+        ++ lib.optional (stdenv.isLinux) pkgs.util-linux
+        ++ lib.optional (stdenv.isDarwin) libtool
+        ++ buildInputs;
 
         inherit nodejs;
 
@@ -631,7 +627,8 @@ let
         meta = {
           # default to Node.js' platforms
           platforms = nodejs.meta.platforms;
-        } // meta;
+        }
+        // meta;
       }
       // extraArgs
     );
@@ -667,15 +664,14 @@ let
       {
         name = "node-dependencies-${name}${if version == null then "" else "-${version}"}";
 
-        buildInputs =
-          [
-            tarWrapper
-            python
-            nodejs
-          ]
-          ++ lib.optional (stdenv.isLinux) utillinux
-          ++ lib.optional (stdenv.isDarwin) libtool
-          ++ buildInputs;
+        buildInputs = [
+          tarWrapper
+          python
+          nodejs
+        ]
+        ++ lib.optional (stdenv.isLinux) pkgs.util-linux
+        ++ lib.optional (stdenv.isDarwin) libtool
+        ++ buildInputs;
 
         inherit dontStrip; # Stripping may fail a build for some package deployments
         inherit dontNpmInstall unpackPhase buildPhase;
@@ -768,13 +764,12 @@ let
       {
         name = "node-shell-${name}${if version == null then "" else "-${version}"}";
 
-        buildInputs =
-          [
-            python
-            nodejs
-          ]
-          ++ lib.optional (stdenv.isLinux) utillinux
-          ++ buildInputs;
+        buildInputs = [
+          python
+          nodejs
+        ]
+        ++ lib.optional (stdenv.isLinux) pkgs.util-linux
+        ++ buildInputs;
         buildCommand = ''
           mkdir -p $out/bin
           cat > $out/bin/shell <<EOF
