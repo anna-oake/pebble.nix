1210268a65e68508c506f40e8a4b30bacbc52305
Anna Oake <anna@oa.ke>
2026-01-16 13:19:32 +0100

    buildPebbleApp: don't produce appstore-bundle.tar.gz

--- buildTools/buildPebbleApp.nix
+++ buildTools/buildPebbleApp.nix
@@ -4,23 +4,12 @@
   pebble-tool,
   system,
 }:
-
 {
   name,
   version,
   src,
   nativeBuildInputs ? [ ],
   postUnpack ? "",
-  type,
-  description,
-  releaseNotes,
-  category ? "",
-  banner ? "",
-  smallIcon ? "",
-  largeIcon ? "",
-  screenshots ? { },
-  homepage ? "",
-  sourceUrl ? "",
   CFLAGS ? "",
   ...
 }@rest:
@@ -46,91 +35,6 @@ let
     url = "https://sdk.core.store/releases/${sdkVersion}/sdk-core.tar.gz";
     sha256 = "0l1gxd9aian6xbpgx64px7pps3215sn1jamav9hjmd86civws2q4";
   };
-
-  stringNotEmpty = str: builtins.isString str && str != "";
-
-  metaYaml =
-    with pkgs.lib;
-    let
-      screenshotPaths = builtins.concatLists (attrValues screenshots);
-      assets =
-        let
-          allDeviceUrls = if screenshots ? all then screenshots.all else [ ];
-          devices = removeAttrs screenshots [ "all" ];
-        in
-        builtins.concatStringsSep "\n" (
-          mapAttrsToList (name: urls: ''
-            - name: ${name}
-              screenshots:
-            ${builtins.concatStringsSep "\n" (map (url: "    - ${url}") urls ++ allDeviceUrls)}
-          '') devices
-        );
-
-      categoryMap = {
-        "Daily" = "5261a8fb3b773043d500000c";
-        "Tools & Utilities" = "5261a8fb3b773043d500000f";
-        "Notifications" = "5261a8fb3b773043d5000001";
-        "Remotes" = "5261a8fb3b773043d5000008";
-        "Health & Fitness" = "5261a8fb3b773043d5000004";
-        "Games" = "5261a8fb3b773043d5000012";
-      };
-      categoryProp = if builtins.hasAttr category categoryMap then categoryMap.${category} else "Faces";
-
-      indentString = str: "  " + (builtins.replaceStrings [ "\n" ] [ "\n  " ] str);
-    in
-    assert asserts.assertMsg (stringNotEmpty name) "name cannot be empty";
-    assert asserts.assertMsg (stringNotEmpty description) "description cannot be empty";
-    assert asserts.assertMsg (stringNotEmpty releaseNotes) "releaseNotes cannot be empty";
-    assert asserts.assertOneOf "type" type [
-      "watchface"
-      "watchapp"
-    ];
-    assert
-      if type == "watchapp" then
-        let
-          validCategories = builtins.attrNames categoryMap;
-        in
-        if elem category validCategories then
-          true
-        else
-          builtins.trace "category must be one of ${generators.toPretty { } validCategories}, but is ${generators.toPretty { } category}" false
-      else
-        true;
-    assert
-      if type == "watchapp" then
-        asserts.assertMsg (stringNotEmpty banner) "banner must point to a file"
-      else
-        true;
-    assert
-      if type == "watchapp" then
-        asserts.assertMsg (stringNotEmpty largeIcon) "largeIcon must point to a file"
-      else
-        true;
-    assert
-      if type == "watchapp" then
-        asserts.assertMsg (stringNotEmpty smallIcon) "smallIcon must point to a file"
-      else
-        true;
-    assert asserts.assertMsg (
-      (builtins.length screenshotPaths) > 0
-    ) "At least 1 screenshot must be provided";
-    builtins.toFile "meta.yml" ''
-      pbw_file: ${name}.pbw
-      header: ${banner}
-      description: |
-      ${indentString description}
-      assets:
-      ${assets}
-      category: ${categoryProp}
-      title: ${name}
-      source: ${sourceUrl}
-      type: ${type}
-      website: ${homepage}
-      release_notes: |
-      ${indentString releaseNotes}
-      small_icon: ${smallIcon}
-      large_icon: ${largeIcon}
-    '';
 in
 pkgsCross.gccStdenv.mkDerivation (
   {
@@ -214,46 +118,16 @@ pkgsCross.gccStdenv.mkDerivation (
       pebble build
     '';
 
-    installPhase =
-      let
-        screenshotPaths = pkgs.lib.flatten (builtins.attrValues screenshots);
-      in
-      ''
-        mkdir -p $out
-        mkdir -p \
-          ${builtins.concatStringsSep " " (map (path: "$out/${dirOf path}") screenshotPaths)} \
-          $out/${dirOf banner} \
-          $out/${dirOf largeIcon} \
-          $out/${dirOf smallIcon}
-
-        cp ${metaYaml} $out/meta.yml
-        cp build/$(basename `pwd`).pbw "$out/${name}.pbw"
-        strip-nondeterminism --type zip "$out/${name}.pbw"
-        ${builtins.concatStringsSep "\n" (map (path: "cp ${path} $out/${path}") screenshotPaths)}
-
-      ''
-      + pkgs.lib.optionalString (stringNotEmpty banner) ''
-        cp ${banner} $out/${banner}
-      ''
-      + pkgs.lib.optionalString (stringNotEmpty largeIcon) ''
-        cp ${largeIcon} $out/${largeIcon}
-      ''
-      + pkgs.lib.optionalString (stringNotEmpty smallIcon) ''
-        cp ${smallIcon} $out/${smallIcon}
-      ''
-      + ''
-
-        cd $out
-        tar --owner=0 --group=0 --numeric-owner --format=gnu \
-            --sort=name --mtime="@0" \
-            -czf appstore-bundle.tar.gz *
-      '';
+    installPhase = ''
+      mkdir -p $out
+      cp build/$(basename `pwd`).pbw "$out/${name}.pbw"
+      strip-nondeterminism --type zip "$out/${name}.pbw"
+    '';
   }
   // (removeAttrs rest [
     "name"
     "src"
     "nativeBuildInputs"
     "postUnpack"
-    "screenshots"
   ])
 )
