8fa23192d7cf1ae5de7ed92f8f8f41e5ffe98870
Anna Oake <anna@oa.ke>
2026-01-17 16:55:05 +0100

    pebble-sdk: build from source

--- derivations/pebble-sdk/default.nix
+++ derivations/pebble-sdk/default.nix
@@ -1,8 +1,82 @@
 {
-  stdenvNoCC,
+  lib,
   pkgs,
+  stdenvNoCC,
+  fetchFromGitHub,
+  gcc-arm-embedded-13,
+  llvmPackages,
+  python3,
+  nodejs,
+  gettext,
+  emscripten,
+  nanopb,
+  which,
+  patchelf,
+  withGabbro ? true,
+  withEmery ? true,
+  withFlint ? true,
+  withDiorite ? true,
+  withChalk ? true,
+  withBasalt ? true,
+  withAplite ? false,
 }:
 let
+  libpebble2 = python3.pkgs.buildPythonPackage {
+    pname = "libpebble2";
+    version = "0.0.30";
+    src = fetchFromGitHub {
+      owner = "pebble-dev";
+      repo = "libpebble2";
+      rev = "6d0e8cffca29eb2ed4a876ea87c50df9c31ad3e7";
+      hash = "sha256-jzN3bMp7hCCFP6wQ4woXTgOmehczvn7cLqen9TlG7Dc=";
+    };
+
+    propagatedBuildInputs = with python3.pkgs; [
+      pyserial
+      six
+      websocket-client
+    ];
+
+    format = "pyproject";
+
+    build-system = with python3.pkgs; [
+      setuptools
+    ];
+  };
+
+  pythonBuildEnv = python3.withPackages (
+    ps: with ps; [
+      pillow
+      freetype-py
+      ply
+      pyusb
+      pyserial
+      sh
+      pypng
+      pexpect
+      cobs
+      svg-path
+      requests
+      gitpython
+      pyelftools
+      pycryptodome
+      mock
+      prompt-toolkit
+      bitarray
+      pep8
+      polib
+      intelhex
+      protobuf
+      grpcio-tools
+      nanopb
+      certifi
+      libclang
+      packaging
+      libpebble2
+      pyftdi
+    ]
+  );
+
   nodeEnv = (pkgs.callPackage ./nodeEnv { }).nodeDependencies;
   pythonEnv = pkgs.python3.withPackages (
     ps: with ps; [
@@ -12,38 +86,119 @@ let
     ]
   );
 in
+assert lib.asserts.assertMsg (!withAplite) "aplite is not supported yet";
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "pebble-sdk";
-  version = "4.9.77";
+  version = "4.9.109";
 
-  src = fetchTarball {
-    url = "https://sdk.core.store/releases/${finalAttrs.version}/sdk-core.tar.gz";
-    sha256 = "0l1gxd9aian6xbpgx64px7pps3215sn1jamav9hjmd86civws2q4";
+  src = fetchFromGitHub {
+    owner = "coredevices";
+    repo = "PebbleOS";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-5uI4cPRY8PfbP1GKmvQ16RJty0RWXCZHIRbCTim+kKc=";
+    fetchSubmodules = true;
   };
 
-  nativeBuildInputs = [ pythonEnv ];
+  nativeBuildInputs = [
+    gcc-arm-embedded-13
+    llvmPackages.clang
+    llvmPackages.lld
+    nodejs
+    gettext
+    emscripten
+    nanopb
+    pythonBuildEnv
+    which
+    patchelf
+  ]
+  ++ lib.optionals (stdenvNoCC.hostPlatform.system == "x86_64-linux") [
+    pkgs.gcc_multi
+  ];
 
-  postUnpack = ''
-    TIMESTAMP=1700578963
+  patches = [
+    ./skip-tool-check.patch
+    ./gitinfo-version.patch
+    ./fix-clang-spelling.patch
+    ./skip-npm-install.patch
+  ]
+  ++ lib.optionals (stdenvNoCC.hostPlatform.system == "x86_64-linux") [
+    ./use-gcc-multi-32bit.patch
+  ];
 
-    find "source" -name '._*' -type f -delete
+  postPatch = ''
+    substituteInPlace sdk/waf/wscript \
+      --replace-fail '"zip/waflib.zip", "w",' '"zip/waflib.zip", "w", strict_timestamps=False,'
+    patchShebangs waf third_party/jerryscript/jerryscript/js_tooling
+  '';
 
-    substituteInPlace "source/sdk-core/pebble/common/tools/inject_metadata.py" \
-      --replace-fail "'timestamp' : timestamp," "'timestamp' : $TIMESTAMP," \
-      --replace-fail "RESOURCE_TIMESTAMP_ADDR, '<L', timestamp)" "RESOURCE_TIMESTAMP_ADDR, '<L', $TIMESTAMP)"
+  hardeningDisable = [
+    "zerocallusedregs"
+  ];
 
-    substituteInPlace "source/sdk-core/pebble/common/tools/mkbundle.py" \
-      --replace-fail "generated_at = int(time.time())" "generated_at = $TIMESTAMP" \
-      --replace-fail "socket.gethostname()" "'nix'" \
-      --replace-fail "'timestamp' : firmware_timestamp" "'timestamp' : $TIMESTAMP" \
-      --replace-fail "'timestamp' : resources_timestamp" "'timestamp' : $TIMESTAMP" \
-      --replace-fail "'timestamp': app_timestamp" "'timestamp': $TIMESTAMP" \
-      --replace-fail "'timestamp': worker_timestamp" "'timestamp': $TIMESTAMP"
+  preBuild = ''
+    export HOME="$TMPDIR"
+    export EM_CACHE="$TMPDIR/emscripten-cache"
+    mkdir -p "$EM_CACHE"
+
+    export PEBBLE_GIT_TAG="v${finalAttrs.version}"
+    export PEBBLE_GIT_COMMIT="v${finalAttrs.version}"
+    export PEBBLE_GIT_TIMESTAMP="1700578963"
+    export PEBBLE_SKIP_NPM_INSTALL=1
+
+    unset CC CXX AR AS OBJCOPY LD RANLIB STRIP
+
+    export NANOPB_GENERATOR="${pythonBuildEnv}/bin/python3 $PWD/third_party/nanopb/nanopb/generator/nanopb_generator.py"
+
+    configure_and_build_board() {
+      local board="$1"
+      local platform_name="$2"
+
+      echo "Building for board: $board (platform: $platform_name)"
+
+      ./waf configure --qemu --board "$board" --release --sdkshell
+      ./waf build qemu_image_micro qemu_image_spi
+
+      mkdir -p build/sdk/"$platform_name"/qemu
+      mv build/qemu_micro_flash.bin build/sdk/"$platform_name"/qemu/
+      mv build/qemu_spi_flash.bin build/sdk/"$platform_name"/qemu/
+      mv build/src/fw/tintin_fw.elf build/sdk/"$platform_name"/qemu/"$platform_name"_sdk_debug.elf
+      bzip2 build/sdk/"$platform_name"/qemu/qemu_spi_flash.bin
+    }
+  '';
+
+  buildPhase = lib.concatStringsSep "\n" (
+    [
+      "runHook preBuild"
+    ]
+    ++ lib.optional withGabbro "configure_and_build_board 'spalding_gabbro' 'gabbro'"
+    ++ lib.optional withEmery "configure_and_build_board 'snowy_emery' 'emery'"
+    ++ lib.optional withFlint "configure_and_build_board 'silk_flint' 'flint'"
+    ++ lib.optional withDiorite "configure_and_build_board 'silk_bb2' 'diorite'"
+    ++ lib.optional withChalk "configure_and_build_board 'spalding_bb2' 'chalk'"
+    ++ lib.optional withBasalt "configure_and_build_board 'snowy_bb2' 'basalt'"
+    ++ [
+      "runHook postBuild"
+    ]
+  );
+
+  postBuild = ''
+    python3 build/sdk/waf --help >/dev/null 2>&1 || true
+
+    mkdir -p sdk-core
+    cp -r build/sdk sdk-core/pebble
+
+    mv sdk-core/pebble/package.json sdk-core/
+    mv sdk-core/pebble/use_requirements.json sdk-core/
+    mv sdk-core/pebble/requirements.txt sdk-core/
+
+    requirements="$(cat sdk-core/use_requirements.json)"
+    printf '{\n  "requirements": %s,\n  "version": "%s",\n  "type": "sdk-core",\n  "channel": ""\n}\n' \
+      "$requirements" \
+      "${finalAttrs.version}" \
+      > sdk-core/manifest.json
   '';
 
   installPhase = ''
-    python3 sdk-core/pebble/waf --help >/dev/null 2>&1 || true
-
     SDK_PATH="$out/current"
     mkdir -p $SDK_PATH
 
